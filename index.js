0e400e28d749268123b4bc34b6ce1e91a4bb198d
Giuliana Bruno <giulianabruno616@gmail.com>
2023-05-01 03:27:50 -0300

    Diseño de la aplicacion con css y responsive design junto con la funcionalidad de enciptar un texto

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-if(texto[i] == 'a'){
-    out += traduccion["a"] ;
-}
-else if(texto[i] == 'e'){
-    out += traduccion["e"];
-}
-else if(texto[i] == 'i'){
-    out += traduccion["i"]; 
-}
-else if(texto[i] == 'o'){
-    out += traduccion["o"]; 
-}
-else if(texto[i] == 'u'){
-    out += traduccion["u"]; 
-}
-else{
-    out += texto[i];
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let munheco = document.getElementById("munheco");
+
+
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+ if (texto.length != 0) {
+    let texto = document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    munheco.src = "./img/encriptado.jpg";
+ } else {
+    munheco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes ingresar algún texto");
+ }
 }
 
-function demoDisplay() {
-    document.getElementById("myP1").style.display = "none";
-}               
-function demoVisibility() {
-    document.getElementById("myP2").style.visibility = "hidden";
+function copiar(){
+    mensaje.select();
+    document.exeCommand("copy");
 }
-
